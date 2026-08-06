0f2f7c766ef80de71c583377e3f9aa35211d917c
spamyouracc-spec <spamyouracc@gmail.com>
2026-08-06 11:34:35 +0530

    merge

--- salary.py
+++ salary.py
@@ -1,10 +1,8 @@
 def basic_pay(hours, rate):
     return hours * rate
 
-
 def add_hra(basic, percent):
     return basic + (basic * percent / 100.0)
-
 
 def deduct_tax(salary, percent):
     return salary - (salary * percent / 100.0)
@@ -13,3 +11,4 @@ if __name__ == "__main__":
     hours = 160
     rate = 250
     print(basic_pay(hours, rate))
+    
