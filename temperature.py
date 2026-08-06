ca399e7e96866b291f4909da7a1bdf0949ba96e0
spamyouracc-spec <spamyouracc@gmail.com>
2026-08-06 11:20:04 +0530

    feature fahrenheit_to_celcius

--- temperature.py
+++ temperature.py
@@ -1,6 +1,12 @@
 def celsius_to_fahrenheit(c):
+    """Convert Celsius to Fahrenheit and return the result."""
     return (c * 9/5) + 32
+
+def fahrenheit_to_celsius(f):
+    """Convert Fahrenheit to Celsius and return the result."""
+    return (f - 32) * 5/9
 
 
 if __name__ == "__main__":
     print(celsius_to_fahrenheit(25))
+    print(fahrenheit_to_celsius(77))
