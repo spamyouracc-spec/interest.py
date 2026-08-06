8a0d4885e6641433fc5c0fbdc7d6484af16702e0
spamyouracc-spec <spamyouracc@gmail.com>
2026-08-06 11:13:59 +0530

    feature_cube

--- even.py
+++ even.py
@@ -1,6 +1,14 @@
+def is_even(n):
+    return n % 2 == 0
+
+def square(n):
+    return n * n
+
 def cube(n):
     return n * n * n
 
 
 if __name__ == "__main__":
+    print(is_even(10))
+    print(square(10))
     print(cube(10))
