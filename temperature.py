04830bad49bcfcc16f2463f5115f0212e2141236
spamyouracc-spec <spamyouracc@gmail.com>
2026-08-06 11:21:06 +0530

    feature celsius to kelvin

--- temperature.py
+++ temperature.py
@@ -1,12 +1,16 @@
 def celsius_to_fahrenheit(c):
-    """Convert Celsius to Fahrenheit and return the result."""
+
     return (c * 9/5) + 32
 
 def fahrenheit_to_celsius(f):
-    """Convert Fahrenheit to Celsius and return the result."""
     return (f - 32) * 5/9
+
+def celsius_to_kelvin(c):
+    return c + 273.15
 
 
 if __name__ == "__main__":
+
     print(celsius_to_fahrenheit(25))
     print(fahrenheit_to_celsius(77))
+    print(celsius_to_kelvin(25))
